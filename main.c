4ddcf992fad477ddb492eb50c643883394d97d06
Yoonsahng Kim <39370616+yyoonsahng@users.noreply.github.com>
2020-07-01 21:07:42 +0900

    Update main.c

--- main.c
+++ main.c
@@ -6,9 +6,9 @@ void gotoxy(int x, int y)
 }
 int main() {
 	int start = 0;
-	system("mode con cols=210 lines=105");
+	system("mode con cols=210 lines=105");//화면 크기를 지정한다.
 	while (start != 4) {
-		start = menu();
+		start = menu();//게임이 계속 반복 될 수 있도록
 	}
 	return 0;
 }
